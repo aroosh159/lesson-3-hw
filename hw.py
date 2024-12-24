3de13e50994623a5100fca57bfc691a22cf18206
aroosh159 <arooshalisharif@gmail.com>
2024-12-24 16:26:55 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,12 @@
+#exam eligibilty
+print("Checking exam eligibility of a student")
+num = int(input("Write youe attendance:"))
+sick = (input("were you ill"))
+if num>70: 
+    print("accepted,as attendace is",num,"Check other conditions")
+    if sick>yes: # type: ignore
+       print("Accepted")
+    else:
+       print("Rejected")
+else :
+ print("Rejected")
